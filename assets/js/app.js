8c34a1ff92f007cef8725dfe73b08f7ced1e4bfe
Emmanuel <cmugarura@gmail.com>
2018-05-11 18:48:48 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -308,7 +308,7 @@ $.getJSON("data/DOITT_MUSEUM_01_13SEPT2010.geojson", function (data) {
 
 map = L.map("map", {
   zoom: 10,
-  center: [40.702222, -73.979378],
+  center: [-1.863986, 29.917346],
   layers: [cartoLight, boroughs, markerClusters, highlight],
   zoomControl: false,
   attributionControl: false
